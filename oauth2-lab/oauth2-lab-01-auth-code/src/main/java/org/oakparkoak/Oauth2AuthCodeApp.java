72f1738e7826d1093f3141272db048ad630cecdb
ElijahDu <withxlaser@gmail.com>
2021-02-09 11:02:36 +0800

    :sparkles: complete auth code demo

---
+++ oauth2-lab/oauth2-lab-01-auth-code/src/main/java/org/oakparkoak/Oauth2AuthCodeApp.java
@@ -0,0 +1,16 @@
+package org.oakparkoak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @package: org.oakparkoak
+ * @author: Captain
+ * @time: 2/3/2021 6:26 PM
+ */
+@SpringBootApplication
+public class Oauth2AuthCodeApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2AuthCodeApp.class, args);
+    }
+}
---
+++ oauth2-lab/oauth2-lab-01-auth-code/src/main/java/org/oakparkoak/controller/UserController.java
@@ -0,0 +1,34 @@
+package org.oakparkoak.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.oakparkoak.model.UserA;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @package: org.oakparkoak.api
+ * @author: Captain
+ * @time: 2/3/2021 6:25 PM
+ */
+@Slf4j
+@RestController
+public class UserController {
+    @GetMapping("/api/u")
+    public ResponseEntity<UserA> getUser() {
+        User principal = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        log.info("Principal: {}", principal);
+
+        UserA user = new UserA();
+        user.setUsername(principal.getUsername());
+        user.setEmail("captain@gmail.com");
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/callback")
+    public String callback() {
+        return "ok";
+    }
+}
